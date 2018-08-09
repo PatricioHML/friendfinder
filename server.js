e54f137482cf63c8655ce85d85768a06d91ed5a0
PatricioHML <patriciohml@hotmail.com>
2018-08-08 21:19:39 -0500

    server

--- server.js
+++ server.js
@@ -1,2 +1,39 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
 
+var PORT = 3000;
 
+// aquí voy a copiar todo lo de body parser
+
+// create application/json parser
+var jsonParser = bodyParser.json()
+
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+// POST /login gets urlencoded bodies
+app.post('/login', urlencodedParser, function (req, res) {
+    if (!req.body) return res.sendStatus(400)
+    res.send('welcome, ' + req.body.username)
+})
+
+// POST /api/users gets JSON bodies
+app.post('/api/users', jsonParser, function (req, res) {
+    if (!req.body) return res.sendStatus(400)
+    // create user in req.body
+})
+
+// parse various different custom JSON types as JSON
+app.use(bodyParser.json({ type: 'application/*+json' }))
+
+// parse some custom thing into a Buffer
+app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }))
+
+// parse an HTML body into a string
+app.use(bodyParser.text({ type: 'text/html' }))
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT: " + PORT)
+
+});
